1ccf6ab00b7aa3d08d27fa72927a7e6f7bb92a24
Fernando Talavera <ftalaveram01@gmail.com>
2025-06-28 01:41:40 +0200

    primera maqueta de portfolio con react

---
+++ portfolio/src/constantes/tecnologias.js
@@ -0,0 +1,36 @@
+export function getTecnologias() {
+    return [
+        {
+            nombre: 'HTML',
+            logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg'
+        },
+        {
+            nombre: 'CSS',
+            logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg'
+        },
+        {
+            nombre: 'JavaScript',
+            logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg'
+        },
+        {
+            nombre: 'React',
+            logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg'
+        },
+        {
+            nombre: 'Git',
+            logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg'
+        },
+        {
+            nombre: 'GitHub',
+            logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg'
+        },
+        {
+            nombre: 'MySQL',
+            logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg'
+        },
+        {
+            nombre: 'Node.js',
+            logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg'
+        }
+    ]
+}
