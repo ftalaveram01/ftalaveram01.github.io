8d3877fb6e060a28ac3f01efd6232df368f12b0a
Fernando Talavera <ftalaveram01@gmail.com>
2025-06-29 18:56:11 +0200

    nueva informacion en sobre mi

--- src/components/SobreMiCard.jsx
+++ src/components/SobreMiCard.jsx
@@ -2,8 +2,12 @@ export default function SobreMiCard() {
     return (
         <section className='tarjeta'>
             <h2>Sobre mí</h2>
-            <h4>Programador Junior</h4>
-            <p>Conocimiento en desarrollo sostenible, mantenible en el tiempo y trabajo en equipo</p>
+            <div className="sobreMiTexto">
+                <h4>Programador Junior español en busca de oportunidades</h4>
+                <p>Conocimiento en desarrollo sostenible, mantenible en el tiempo y trabajo en equipo</p>
+                <h4>Apasionado por el desarrollo web y móvil</h4>
+                <p>Desarrollo web por medio de frameworks modernos y desarrollo Android Java</p>
+            </div>
         </section>
     )
 }
